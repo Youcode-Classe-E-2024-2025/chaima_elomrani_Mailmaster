50792b452d0c4703b3b2406336e14d3db9be1c27
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-04-07 12:14:00 +0100

    added the register method in the controllers

--- Backend/app/Http/Controllers/AuthController.php
+++ Backend/app/Http/Controllers/AuthController.php
@@ -3,8 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
 
 class AuthController extends Controller
 {
-    //
+
+    protected $authService;
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    
+    public function __construct($authService){
+
+        $this->authService = $authService; 
+    }
+
+
+    public function register (Request $request){
+        $validator = Validator::make($request->all(),[
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors());
+        }
+
+        $user = $this->authService->register($request->all());
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ]);
+
+    }
 }
+
