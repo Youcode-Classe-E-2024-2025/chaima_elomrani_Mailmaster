c0309b700689e704d4dc1fb5fdf1e718cf37068e
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-04-10 16:28:11 +0100

    added method index and show in the controllers using in them the methods getallsub and getsubbyid in the services

--- Backend/app/Http/Controllers/SubscribersController.php
+++ Backend/app/Http/Controllers/SubscribersController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\SubscriberService;
 use Illuminate\Http\Request;
 
 class SubscribersController extends Controller
 {
-    //
+    protected $subscriberService;
+
+    public function __construct(SubscriberService $subscriberService){
+
+        $this->subscriberService = $subscriberService;
+    }
+
+    public function index(Request $request){
+        $subscribers = $this->subscriberService->getAllSubscribers();
+        return response()->json($subscribers);
+    }
+
+    public function show($id){
+        $subscriber = $this->subscriberService->getSubscriberById($id);
+        return response()->json($subscriber);
+    }
+
+
 }
