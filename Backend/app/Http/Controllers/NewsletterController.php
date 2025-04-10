e1c8de95a86a47b6acb624f3ac52d976629e0b5d
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-04-10 16:32:59 +0100

    added the newsletter CRUD in the controllers

--- Backend/app/Http/Controllers/NewsletterController.php
+++ Backend/app/Http/Controllers/NewsletterController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\NewsletterService;
 use Illuminate\Http\Request;
 
 class NewsletterController extends Controller
 {
-    //
+    protected $newsletterService;
+
+    public function __construct(NewsletterService $newsletterService)
+    {
+        $this->newsletterService = $newsletterService;
+    }
+
+    public function index(Request $request)
+    {
+        $newsletters = $this->newsletterService->getAllNewsletters();
+        return response()->json($newsletters);
+    }
+    public function show($id)
+    {
+        $newsletter = $this->newsletterService->getNewsletterById($id);
+        return response()->json($newsletter);
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $newsletter = $this->newsletterService->createNewsletter($data);
+        return response()->json($newsletter, 201);
+    }
+    public function update(Request $request, $id)
+    {
+        $data = $request->all();
+        $newsletter = $this->newsletterService->updateNewsletter($id, $data);
+        return response()->json($newsletter);
+    }
+    public function destroy($id)
+    {
+        $newsletter = $this->newsletterService->deleteNewsletter($id);
+        return response()->json(null, 204);
+    }
 }
