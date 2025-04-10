d3cf9dbf23c0286d628e3af527a27329001cf62e
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-04-10 17:25:18 +0100

    documenting routes

--- Backend/app/Http/Controllers/NewsletterController.php
+++ Backend/app/Http/Controllers/NewsletterController.php
@@ -14,6 +14,14 @@ class NewsletterController extends Controller
         $this->newsletterService = $newsletterService;
     }
 
+    /**
+     * Display a listing of the newsletters.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+
+
     public function index(Request $request)
     {
         $newsletters = $this->newsletterService->getAllNewsletters();
@@ -25,18 +33,43 @@ class NewsletterController extends Controller
         return response()->json($newsletter);
     }
 
+
+
+    /**
+     * Store a newly created newsletter in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
         $data = $request->all();
         $newsletter = $this->newsletterService->createNewsletter($data);
         return response()->json($newsletter, 201);
     }
+
+    
+    /**
+     * Update the specified newsletter in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
     public function update(Request $request, $id)
     {
         $data = $request->all();
         $newsletter = $this->newsletterService->updateNewsletter($id, $data);
         return response()->json($newsletter);
     }
+
+    /**
+     * Remove the specified newsletter from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function destroy($id)
     {
         $newsletter = $this->newsletterService->deleteNewsletter($id);
--- Backend/app/Http/Controllers/SubscribersController.php
+++ Backend/app/Http/Controllers/SubscribersController.php
@@ -14,21 +14,51 @@ class SubscribersController extends Controller
         $this->subscriberService = $subscriberService;
     }
 
+    /**
+     * Retrieve all subscribers
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function index(Request $request){
         $subscribers = $this->subscriberService->getAllSubscribers();
         return response()->json($subscribers);
     }
 
+
+    /**
+     * Display the specified subscriber.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function show($id){
         $subscriber = $this->subscriberService->getSubscriberById($id);
         return response()->json($subscriber);
     }
 
+
+    /**
+     * Store a newly created subscriber in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function store(Request $request){
         $data = $request->all();
         $subscriber = $this->subscriberService->createSubscriber($data);
         return response()->json($subscriber, 201);
     }
+
+
+
+    /**
+     * Update the specified subscriber in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
 
     public function update(Request $request, $id){
         $data = $request->all();
@@ -36,6 +66,14 @@ class SubscribersController extends Controller
         return response()->json($subscriber);
     }
 
+
+    /**
+     * Remove the specified subscriber from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    
     public function destroy($id){
         $subscriber = $this->subscriberService->deleteSubscriber($id);
         return response()->json(null, 204);
