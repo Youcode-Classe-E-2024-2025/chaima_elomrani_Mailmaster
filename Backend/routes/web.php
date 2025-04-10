12d81c79acf984072df274f592fb180703cb5729
chaimaa-elomrani <chaimaelomrani6@gmail.com>
2025-04-10 19:41:25 +0100

    added subscribers controller routes

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\SubscribersController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+// subbscribers routes
+Route::get('/subscribers',[SubscribersController::class,'index']);
+Route::get('/subscribers/{id}',[SubscribersController::class,'show']);
+Route::post('/subscribers',[SubscribersController::class,'store']);
+Route::put('/subscribers/{id}',[SubscribersController::class,'update']);
+Route::delete('/subscribers/{id}',[SubscribersController::class,'destroy']);
